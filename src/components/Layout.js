4e97a3eb8300fc6de567048ced19c18ee2bb6dc6
mari7102 <55129539+mari7102@users.noreply.github.com>
2020-01-24 07:34:31 +0100

    Chat-app

---
+++ src/components/Layout.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client'
+import { USER_CONNECTED } from '../Events'
+
+const socketUrl = "http://10.131.164.188:3231"
+export default class Layout extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            socket:null,
+            user:null
+        };
+    }
+
+    componentWillMount(){
+        this.initSocket()
+    }
+
+    initSocket = ()=>{
+         const socket = io(socketUrl)
+         socket.on('connect', ()=>{
+             console.log("Connected");
+         })
+         this.setState({socket})
+    }
+
+    setUser = (user)=>{
+        const {socket} = this.state
+        socket.emit(USER_CONNECTED, user);
+        this.setState({user})
+    }
+
+    logout = ()=>{
+        const { socket } = this.state
+        socket.emit(LOGOUT)
+        this.setState({user:null})
+    }
+    
+
+    render(){
+        const { title } = this.props
+        return(
+
+            <div className="container">
+                {title}
+            </div>
+
+        );
+    }
+}
